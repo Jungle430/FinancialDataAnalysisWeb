2a7a2d20d9b5199fcf6342ac8e13b37e9e75596e
Jungle430 <junglece430@gmail.com>
2024-12-06 22:14:05 +0800

    ignore(*): config file mod

---
+++ src/views/Login/index.vue
@@ -0,0 +1,72 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <header class="login-header">
+      <div class="container m-top-20">
+        <h1 class="logo">
+          <RouterLink to="/">小兔鲜</RouterLink>
+        </h1>
+        <RouterLink class="entry" to="/">
+          进入网站首页
+          <i class="iconfont icon-angle-right" />
+          <i class="iconfont icon-angle-right" />
+        </RouterLink>
+      </div>
+    </header>
+    <section class="login-section">
+      <div class="wrapper">
+        <nav>
+          <a href="javascript:;">账户登录</a>
+        </nav>
+        <div class="account-box">
+          <div class="form">
+            <el-form
+              ref="form"
+              label-position="right"
+              label-width="60px"
+              status-icon
+              hide-required-asterisk>
+              <el-form-item label="账户" prop="userName">
+                <el-input  />
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input
+                  type="password"
+                  show-password />
+              </el-form-item>
+              <el-form-item label-width="22px" prop="TOS">
+                <el-checkbox size="large">
+                  我已同意隐私条款和服务条款
+                </el-checkbox>
+              </el-form-item>
+              <el-button size="large" class="subBtn">
+                点击登录
+              </el-button>
+            </el-form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="login-footer">
+      <div class="container">
+        <p>
+          <a href="javascript:;">关于我们</a>
+          <a href="javascript:;">帮助中心</a>
+          <a href="javascript:;">售后服务</a>
+          <a href="javascript:;">配送与验收</a>
+          <a href="javascript:;">商务合作</a>
+          <a href="javascript:;">搜索推荐</a>
+          <a href="javascript:;">友情链接</a>
+        </p>
+        <p>CopyRight &copy; 小兔鲜儿</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
