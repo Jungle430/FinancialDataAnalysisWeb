12618fd38525e20a17fab1cc19d0335273c6080d
Jungle430 <junglece430@gmail.com>
2024-12-07 22:45:24 +0800

    feat(*): mod router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login/index.vue';
+import Home from "@/views/Home/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: 'home',
+      component: Home
+    },
+    {
+      path: "/login",
+      name: 'login',
+      component: Login
+    }
   ],
 })
 
