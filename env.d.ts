4d099e3e1a630706aca14a783a9435fe3a88a37e
Jungle430 <junglece430@gmail.com>
2024-12-25 23:45:55 +0800

    feat(*): add env file

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
+/// <reference types="node" />
+
 declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
+
+declare global {
+  interface ImportMetaEnv {
+    VITE_API_URL: string;
+  }
+}
+
+export { };
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,7 +5,7 @@ import { message } from 'ant-design-vue';
 const HEADERS_TOKEN_KEY: string = 'X-Token'
 
 const service = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000
 })
 
